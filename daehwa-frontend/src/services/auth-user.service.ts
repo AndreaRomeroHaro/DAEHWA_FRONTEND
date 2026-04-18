a969c439258ac9a239a7fac71211bf23e926f945
AndreaRH <xaromhar009@ieshnosmachado.org>
2026-04-19 00:17:33 +0200

    hecho authuserservice para autenticacion

---
+++ daehwa-frontend/src/services/auth-user.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject,tap } from "rxjs";
+
+@Injectable({
+    providedIn:'root'
+})
+export class AuthUserService{
+    private apiUrl='http://127.0.0.1:8000/api/login/'
+
+    private usuarioSubject = new BehaviorSubject<any>(null);
+    usuario$=this.usuarioSubject.asObservable();
+
+    constructor (private http: HttpClient){
+
+    }
+
+    login(email:string, password:string){
+        return this.http.post<any>(this.apiUrl,{email,password}).pipe
+            (tap(res=>{localStorage.setItem('token',res.token);
+                    this.usuarioSubject.next(res.usuario);
+                    localStorage.setItem('usuario',JSON.stringify(res.usuario));
+            })
+        );
+    }
+
+    cargarUsuarioDesdeStorage(){
+        const usuario=localStorage.getItem('usuario');
+        if(usuario){
+            this.usuarioSubject.next(JSON.parse(usuario))
+        }
+    }
+
+    obtenerUsuarioActual(){
+        return this.usuarioSubject.value;
+    }
+
+    logout(){
+        localStorage.removeItem('token');
+        localStorage.removeItem('usuario');
+        this.usuarioSubject.next(null);
+    }
+
+    estaLogeado():boolean{
+        return !!localStorage.getItem('token');
+    }
+}
